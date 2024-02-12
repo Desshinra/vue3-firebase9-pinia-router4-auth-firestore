b280726de0add7001b5f500f927e72ac2ab04918
Amado Dubon <desshinra@gmail.com>
2024-02-12 15:05:34 -0600

    Fisrt commit: vite, routerlink, pinia

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <p>{{ mayusText }}</p>
+
+    </div>
+</template>
+
+<script setup>
+import { useUserStore } from '../stores/user'
+import {computed} from 'vue'
+    
+const userStore = useUserStore();
+const mayusText = computed(() => userStore.userData.toLocaleUpperCase())
+</script>
